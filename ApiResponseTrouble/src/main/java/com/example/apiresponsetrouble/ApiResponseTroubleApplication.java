4b9d3227c6ba65350af6bb8620714381ed4b3dc4
Jindnjs <wlsdnjs0310@naver.com>
2025-06-26 19:01:29 +0900

    initial commit

---
+++ ApiResponseTrouble/src/main/java/com/example/apiresponsetrouble/ApiResponseTroubleApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiresponsetrouble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResponseTroubleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiResponseTroubleApplication.class, args);
+    }
+
+}
---
+++ ApiResponseTrouble/src/test/java/com/example/apiresponsetrouble/ApiResponseTroubleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiresponsetrouble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResponseTroubleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
